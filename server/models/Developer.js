11610c8379cb96d650577e8a3af449fe50558c10
Izzy <browncans@gmail.com>
2021-08-23 19:11:25 -0700

    tab spacing

--- server/models/Developer.js
+++ server/models/Developer.js
@@ -1,59 +1,58 @@
-const { Schema, model } = require('mongoose');
-const { bcrypt } = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const { bcrypt } = require("bcrypt");
 // const developerFormat = require('../utils/developerFormat');
 
 const developerSchema = new Schema({
-    name: {
-        type: String,
-        required: 'We know you have a name!',
-        trim: true,
+  name: {
+    type: String,
+    required: "We know you have a name!",
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email address~`,
     },
+    trim: true,
+  },
 
-    email: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function (v) {
-                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
-            },
-            message: props => `${props.value} is not a valid email address~`
-        },
-        trim: true,
-    },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
 
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-    },
+  githubName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-    githubName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-
-    Job: {
-        type: Schema.Types.ObjectId,
-        ref: 'Employer',
-        required: true,
-    }
-
+  Job: {
+    type: Schema.Types.ObjectId,
+    ref: "Employer",
+    required: true,
+  },
 });
 
 developerSchema.pre("save", async function (next) {
-    if (this.isNew || this.isModified("password")) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-    next();
+  next();
 });
 
 developerSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
-const Developer = model('Developer', developerSchema);
+const Developer = model("Developer", developerSchema);
 
 module.exports = Developer;
