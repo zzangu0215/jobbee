83cfbd42d125f40f243907b5daa2e4052bfcc4e9
Junny <tajo0215@gmail.com>
2021-08-26 15:06:09 -0700

    job lists of an employer, update job modal implemented

---
+++ client/src/pages/Employer-jobs/Employer-job-card.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+import Modal from "@material-tailwind/react/Modal";
+import ModalHeader from "@material-tailwind/react/ModalHeader";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+import Button from "@material-tailwind/react/Button";
+
+import UpdateModal from "../../components/Update-modal/Update-modal";
+
+const EmpJobCard = ({
+  createdAt,
+  companyName,
+  listingName,
+  website,
+  description,
+}) => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className="md:flex-1 px-10 mt-8">
+      <div className="bg-white  mx-auto rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
+        <div className="mt-2">
+          <a href={website}>
+            <h1 className="text-xl text-gray-700 font-semibold hover:underline cursor-pointer">
+              {companyName}
+            </h1>
+          </a>
+          <div className="flex mt-3">
+            <h2 className="text-md text-gray-700 font-semibold">
+              {listingName}
+            </h2>
+          </div>
+          <p className="mt-4 text-md text-gray-600">{description}</p>
+          <div className="flex justify-between items-center">
+            <div className="mt-4 flex items-center space-x-4 py-6">
+              <div className="text-sm font-semibold">
+                <span> Posted Date: {createdAt}</span>
+              </div>
+            </div>
+            <button
+              className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => setShowModal(true)}
+            >
+              Update
+            </button>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
+              Delete
+            </button>
+            <Modal
+              size="regular"
+              active={showModal}
+              toggler={() => setShowModal(false)}
+            >
+              <ModalHeader toggler={() => setShowModal(false)}>
+                Update your job!
+              </ModalHeader>
+              <ModalBody>
+                <UpdateModal />
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="red"
+                  buttonType="link"
+                  onClick={(e) => setShowModal(false)}
+                  ripple="dark"
+                >
+                  Close
+                </Button>
+
+                <Button
+                  color="green"
+                  onClick={(e) => setShowModal(false)}
+                  ripple="light"
+                >
+                  Confirm
+                </Button>
+              </ModalFooter>
+            </Modal>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmpJobCard;
--- client/src/pages/Employer-jobs/Employer-jobs.js
+++ client/src/pages/Employer-jobs/Employer-jobs.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 
-import JobListCard from "../../components/Job-List-card/Job-List-card";
-import { FiSearch } from "react-icons/fi";
+import EmpJobCard from "./Employer-job-card";
 
 import "./Employer-jobs.css";
 import { QUERY_JOBS } from "../../utils/queries";
@@ -15,21 +14,11 @@ const EmployerJobs = () => {
 
   return (
     <>
-      <div className="mt-8 flex justify-center emp-jobs">Job Lists</div>
-      <div className="mt-4 mb-4 relative flex justify-center text-gray-600">
-        <input
-          type="search"
-          name="serch"
-          placeholder="Search by Position..."
-          className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
-        />
-        <button type="submit">
-          <FiSearch size={30} className="right-0" />
-        </button>
-      </div>
+      <div className="mt-8 flex justify-center emp-jobs">Your Jobs</div>
+
       <div className="min-h-screen pb-8 bg-gray-100 md:flex items-center md:justify-center">
         {jobs.map((job) => (
-          <JobListCard
+          <EmpJobCard
             listingName={job.listingName}
             companyName={job.companyName}
             key={job._id}
