b48ed51c7fae88b7fb4f318875d67ba4de08fdce
Junny <tajo0215@gmail.com>
2021-08-27 09:41:02 -0700

    comment out the codes don't need

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -149,6 +149,7 @@ export default function Navbar() {
                           {({ active }) => (
                             <Link
                               onClick={logout}
+                              to="/"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
