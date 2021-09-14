181e3accaec49d56a296a0369908336f8e9479e6
maxd66 <max.dunn63@gmail.com>
2021-09-13 19:10:40 -0700

    added ability to submit login and signup forms via enter

--- client/src/pages/Employer-Signup/Employer-Signup.js
+++ client/src/pages/Employer-Signup/Employer-Signup.js
@@ -208,8 +208,7 @@ function EmployerSignUp() {
               <div className="flex items-center justify-between">
                 <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                  onClick={handleFormSubmit}
+                  type="submit"
                 >
                   Sign Up
                 </button>
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -41,7 +41,10 @@ function Login() {
               </p>
             </div>
           ) : (
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form
+              onSubmit={handleFormSubmit}
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            >
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -77,8 +80,7 @@ function Login() {
               <div className="flex items-center justify-between">
                 <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                  onClick={handleFormSubmit}
+                  type="submit"
                 >
                   Login
                 </button>
